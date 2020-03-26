00c922e05f140720a3f55c1b95b1da947062a0a3
shivamkulkarni <shivamkulkarnimu10@gmail.com>
2020-03-26 02:02:02 -0500

    Update and rename cnn-handwritten-digits.py to cnn-fashion.py

--- Convolutional-Neural-Network/cnn-fashion.py
+++ Convolutional-Neural-Network/cnn-fashion.py
@@ -1,25 +1,28 @@
-from keras.datasets import mnist
+import numpy
+from keras.datasets import fashion_mnist
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Dense, Flatten
 from keras.utils import np_utils
-from wandb.keras import WandbCallback
 import wandb
+from wandb.wandb_keras import WandbKerasCallback
 
+# logging code
 run = wandb.init()
 config = run.config
-config.img_width = 28
-config.img_height = 28
 config.first_layer_conv_width = 3
 config.first_layer_conv_height = 3
 config.dense_layer_size = 100
 config.epochs = 10
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-X_train = X_train.astype('float32')
-X_train /= 255.
-X_test = X_test.astype('float32')
-X_test /= 255.
+# load data
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+config.img_width = X_train.shape[1]
+config.img_height = X_train.shape[2]
+
+labels =["T-shirt/top","Trouser","Pullover","Dress",
+    "Coat","Sandal","Shirt","Sneaker","Bag","Ankle boot"]
 
 #reshape input data
 X_train = X_train.reshape(X_train.shape[0], config.img_width, config.img_height, 1)
@@ -28,25 +31,23 @@ X_test = X_test.reshape(X_test.shape[0], config.img_width, config.img_height, 1)
 # one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
-labels=range(10)
 
-# build model
-model = Sequential()
+num_classes = y_train.shape[1]
+
+# create model
+model=Sequential()
 model.add(Conv2D(32,
     (config.first_layer_conv_width, config.first_layer_conv_height),
-    input_shape=(28, 28,1),
+    input_shape=(config.img_width, config.img_height,1),
     activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(config.dense_layer_size, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
-
 model.compile(loss='categorical_crossentropy', optimizer='adam',
                 metrics=['accuracy'])
-
 model.summary()
 
-model.fit(X_train, y_train, validation_data=(X_test, y_test),
-        epochs=config.epochs,
-        callbacks=[WandbCallback(data_type="image")])
+# Fit the model
+model.fit(X_train, y_train, epochs=config.epochs, validation_data=(X_test, y_test),
+                    callbacks=[WandbKerasCallback(data_type="image", labels=labels)])
